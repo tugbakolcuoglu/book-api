c781c2604a78408cc9d82522e3f54b2caa7f0cd4
kasimpasaoglu <kasimpasaoglu@windowslive.com>
2026-02-20 16:24:02 +0300

    feat: add CreateBookRequest model and update Book type to Mystery

--- WebApplication1/Controllers/BookControllers.cs
+++ WebApplication1/Controllers/BookControllers.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Controllers.VMs;
 using WebApplication1.Interfaces;
+using WebApplication1.Service.DTOs;
 
 namespace WebApplication1.Controllers;
 [ApiController]
@@ -32,4 +33,33 @@ public class BooksController(IBookService bookService) : ControllerBase
         var serviceResult = await bookService.GetBookByIdAsync(id);
         return Ok(serviceResult);
     }
+    
+    // TODO: Create, Delete Endpointleri implemente edilecek.
+
+    // [HttpPost]
+    // public async Task<IActionResult> Create(CreateBookRequest request)
+    // {
+    //     var bookDto = new BookDto()
+    //     {
+    //         // id basma isi logic olarak application (servise) konusudur. orda basialcak.
+    //         Title = request.Title,
+    //         Author = request.Author,
+    //         IsAvailable = request.IsAvailable,
+    //         Type = request.Type
+    //     };
+    //     var createResult = await bookService.CreateNewBookAsync(bookDto); // gelen kitap nesnesine Id eklicek, ve repository'e gonderecek. true || false donecek
+    //     if (createResult)
+    //     {
+    //         return Ok();
+    //     }
+    //     return BadRequest();
+    // }
+    
+    // servis katmaninda bu islem icin CreateNewBookAsync metodu olusturulacak. Bu metod gelen kitap nesnesine Id basip repository'e gonderecek.
+    // ONCE INTERFACE (IBookService) EKLENECEK, SONRA SOMUT SINIT (BookService) EKLENECEK
+    // ayni mantik repositoy icin de gecerli.
+    
+    // Doyasiyla kod yazmaya reposiyotu katmanidan baslanarak yapilirsa hata ihtimali duser.
+    // cunku en icerden baslamis oluruz, disari dgoru (controller) a varmis oluruz.
+
 }
